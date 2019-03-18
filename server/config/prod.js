b415a6a4bd231ab947907208e70146fa8bfc072f
Khang Nguyen <mkavo92@gmail.com>
2019-03-17 20:36:16 -0500

    finish

--- server/config/prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    SECRET: 'Khang',
-    CLOUD_NAME: 'dikyehjqe',
-    API_KEY: '531342213124762',
-    API_SECRET: 'jLTTdmzrr8AyumIVK9l0lg8eNP4',
-    DB_URL: 'mongodb://e-shoe:minhkhang1@ds113845.mlab.com:13845/e-shoe'
-};
